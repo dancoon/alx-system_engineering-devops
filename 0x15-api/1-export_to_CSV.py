326e658d0615305f310b6c0a81e7e686bb6bb0ec
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-07-24 19:33:19 +0300

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+import requests
+import csv
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    to_do = requests.get(url + "todos", params={"userId": user_id}).json()
+    with open("{}.csv".format(user_id), "w") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow([user_id, user.get("username"),
+                          task.get("completed"), task.get("title")])
+         for task in to_do]
